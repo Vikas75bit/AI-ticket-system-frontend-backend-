9f8856fe1f6b1e22dcb473956ca0258b1bb5e170
Vikas75bit <123308531+Vikas75bit@users.noreply.github.com>
2026-06-04 16:43:12 +0530

    Update main.jsx

--- ai-dashboard(frontend)/src/main.jsx
+++ ai-dashboard(frontend)/src/main.jsx
@@ -3,30 +3,79 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Login from "./Login";
 import "./index.css";
+import UserDashboard from "./UserDashboard";
 
 import { useEffect, useState } from "react";
 import { supabase } from "./supabase";
 
 function RootApp() {
   const [session, setSession] = useState(null);
+  const [role, setRole] = useState(null);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+supabase.auth.getSession().then(
+  async ({ data: { session } }) => {
+
+    setSession(session);
+
+    if (session?.user) {
+
+      const { data, error } = await supabase
+        .from("user_roles")
+        .select("*")
+        .eq("id", session.user.id)
+        .single();
+
+      console.log("==========");
+      console.log("User ID:", session.user.id);
+      console.log("Role Data:", data);
+      console.log("Role Error:", error);
+      console.log("==========");
+
+      setRole(data?.role);
+    }
+  }
+);
 
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
+    } = supabase.auth.onAuthStateChange(async (_event, session) => {
       setSession(session);
+
+      if (session?.user) {
+        const { data, error } = await supabase
+          .from("user_roles")
+          .select("*")
+          .eq("id", session.user.id)
+          .single();
+
+        console.log("==========");
+        console.log("User ID:", session.user.id);
+        console.log("Role Data:", data);
+        console.log("Role Error:", error);
+        console.log("==========");
+
+        setRole(data?.role);
+      }
     });
 
     return () => subscription.unsubscribe();
   }, []);
 
+  console.log("Session:", session?.user?.email);
+  console.log("Role:", role);
+
   return (
     <>
-      {session ? <App /> : <Login />}
+    {
+  !session ? (
+    <Login />
+  ) : role === "admin" ? (
+    <App />
+  ) : (
+    <UserDashboard />
+  )
+}
     </>
   );
 }
